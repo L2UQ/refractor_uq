779649c135760051ecf33ababb5a9a1a9fae9f7b
isujonhobbs <54842688+isujonhobbs@users.noreply.github.com>
2023-01-05 03:27:34 -0800

    Script for enabling MERRA aerosol covariance

---
+++ script/make_merra_cov.py
@@ -0,0 +1,25 @@
+# Add MERRA components to retrieval covariance 
+
+import os
+import h5py
+
+covariance_file = os.path.join(os.environ['OCO_CONFIG_DIR'], "retrieval_covariance.h5")
+merra_aer_list = ["DU","SS","BC","OC","SO"]
+
+if os.path.isfile(covariance_file):
+    f = h5py.File(covariance_file,'r+')
+    aercv = f['/aerosol_extinction/gaussian_log/kahn_2b'][:,:]
+    for k in range(len(merra_aer_list)):
+        cvnm = '/aerosol_extinction/gaussian_log/%s' % (merra_aer_list[k])
+        if cvnm in f:
+            s1 = '%s found' % (cvnm)
+            f[cvnm][...] = aercv
+        else:
+            s1 = '%s not found' % (cvnm)
+            f.create_dataset(cvnm,data=aercv)
+        print(s1)
+    f.close()
+    print(aercv)
+else:
+    print('Covariance file not found')
+
---
+++ script/test_config_prior_oco_aer.py
@@ -0,0 +1,67 @@
+# Setup simulation experiment L1b/Met 
+# Based on load_example_config from oco repository
+# Incorporate MERRA aerosol types
+
+import os
+import sys
+
+sys.path.append(os.environ['OCO_CONFIG_DIR'])
+from refractor.factory import process_config
+from refractor import framework as rf
+from retrieval_config import retrieval_config_definition
+import refractor.factory.creator as creator
+
+from pprint import pprint
+import numpy
+import h5py
+import pandas
+import refractor_uq
+
+aerosol_prop_file = os.path.join(os.environ["REFRACTOR_INPUTS"], "l2_aerosol_combined.h5")
+ref_sounding_id = "2020082319555502" 
+l1b_file = "oco2_L1bScND_32686a_200823_B10206r_210506204351.h5"
+met_file = "oco2_L2MetND_32686a_200823_B10206r_210506064119.h5" 
+
+if os.path.exists(l1b_file):
+    print('L1B present')
+else:
+    print('L1B not found')
+
+if os.path.exists(met_file):
+    print('Met present')
+else:
+    print('Met not found')
+
+# Setup config, enable MERRA aerosols
+merra_aer_list = ["DU","SS","BC","OC","SO"]
+config_def = retrieval_config_definition(l1b_file, met_file, ref_sounding_id)
+for j in range(len(merra_aer_list)):
+    curtyp = merra_aer_list[j]
+    config_def['atmosphere']['aerosol'][curtyp] = config_def['atmosphere']['aerosol']['kahn_2b'] 
+    config_def['atmosphere']['aerosol'][curtyp]['properties']['prop_name'] = curtyp
+
+# Select types
+config_def['atmosphere']['aerosol']['aerosols'] = [ "DU", "SO", "water", "ice" ]
+
+config_inst = process_config(config_def)
+
+sv = config_inst['retrieval']['state_vector']
+pprint(sv)
+print(sv.state_vector_name)
+print(sv.state)
+print(sv.state.shape)
+
+# Forward model run
+fm = config_inst['forward_model']
+for spec_idx in range(int(config_inst['common']['num_channels'])):
+    rdspc = fm.radiance(spec_idx,True)
+    wvln = rdspc.spectral_domain.data
+    print(wvln.shape)
+    print(wvln[100:105])
+
+# Write state to CSV
+# Data frame with state vector
+cfrm = pandas.DataFrame({'SVName': sv.state_vector_name, 'SVValue': sv.state})
+csvfl = 'land_state_%s.csv' % (ref_sounding_id)
+cfrm.to_csv(csvfl,index=False,encoding='utf-8')
+
